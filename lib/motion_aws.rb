7505632718282c7473e2654d0f443543d1a85961
Scott Ballantyne <ussballantyne@gmail.com>
2012-10-07 20:00:14 +0800

    adding persistence api

--- lib/motion_aws.rb
+++ lib/motion_aws.rb
@@ -4,9 +4,8 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'foo/*.rb')).each do |file|
     app.files.unshift(file)
   end
-  app.pods do
-    pod 'AWSiOSSDK'
-  end
+  app.vendor_project(File.expand_path(File.join(File.dirname(__FILE__), '../vendor/aws-ios-sdk-1.4.2/AWSiOSSDK.framework')), :static, :products => ['AWSiOSSDK'], :headers_dir => 'Headers')
+  app.vendor_project(File.expand_path(File.join(File.dirname(__FILE__), '../vendor/aws-ios-sdk-1.4.2/AWSPersistence.framework')), :static, :products => ['AWSPersistence'], :headers_dir => 'Headers')
 end
 
 module AWS
